b7f07c8f70e337c72a15234c22479ba8bcaaec8c
Mayakimcodes <mayakimcodes@users.noreply.github.com>
2021-09-27 14:09:37 +0900

    I have added an example of how to use the sorting algorithm

--- queuePractice.cpp
+++ queuePractice.cpp
@@ -8,15 +8,33 @@
 #include<queue>
 using namespace std;
 
-void printQueue(queue<int> que) {
+/*void printQueue(queue<int> que) {
     while (!que.empty()) {
         cout << que.front() << endl;
         que.pop();
     }
-}
+}*/
+
+void selectionSort(int arr[]){
+    for (int i = 0; i < 4; i++) {
+        int min = i;
+        for (int j = i + 1; j < 5; j++) {
+            if (arr[j] < arr[min]){
+                min = j;
+            }
+        }
+
+        if (min != i) {
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+    }
+ }
 
 int main()
 {
+    /* example of basic queue functions
     queue<int> myQueue;
     myQueue.push(1);
     myQueue.push(2);
@@ -28,6 +46,53 @@ int main()
 
     cout << "My queue: " << endl;
     printQueue(myQueue);
+    */
+
+    /* Selection Sort Algo*/
+    /*formula
+    for (i = 0; i < n - 1; i++) {
+        // set current element as the min
+        min = i;
+
+        //check if element is the min
+
+        for (j = i + 1; j < n; j++) {
+            if array[j] < array[min]{
+                min = j;
+            }
+        }
+        //swap the min and current element
+        if (min != i) {
+            swap array[min] and arr[i];
+        }
+    }*/
+
+    int arr[5];
+    cout << "Enter 5 integers: \n";
+
+    //get user input for the array
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    //print out the array elements
+    cout << "The unsorted elements are: \n";
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    selectionSort(arr);
+
+    cout << "The sorted elements are: \n";
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+
+
+
 
     return 0;
 }
