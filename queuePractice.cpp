8465e8a5c034d0febb4c374c61a11c238ed1585e
Mayakimcodes <mayakimcodes@users.noreply.github.com>
2021-09-27 13:47:25 +0900

    Getting queue first and last element and printing all queue elements

--- queuePractice.cpp
+++ queuePractice.cpp
@@ -1,20 +1,34 @@
 // queuePractice.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// A queue is a a FIFO (first in first out) data structure
+// Example: people standing in front of a line get served first or using a printer
+// or online orders
 //
 
 #include <iostream>
+#include<queue>
+using namespace std;
+
+void printQueue(queue<int> que) {
+    while (!que.empty()) {
+        cout << que.front() << endl;
+        que.pop();
+    }
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    queue<int> myQueue;
+    myQueue.push(1);
+    myQueue.push(2);
+    myQueue.push(3);
+
+    cout << "Size is " << myQueue.size() << endl;
+    cout << "First element is " << myQueue.front() << endl;
+    cout << "Last element is " << myQueue.back() << endl;
+
+    cout << "My queue: " << endl;
+    printQueue(myQueue);
+
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
